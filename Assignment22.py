16e8174547a5a78cb465a9b8e226a60ef5140429
Vivek Kumar <vivekgupta70509@gmail.com>
2025-04-30 22:39:05 +0530

    Add files via upload

---
+++ Assignment22.py
@@ -0,0 +1,48 @@
+"""
+
+#Q1
+n=int(input("Enter value of n"))
+sum=0
+for e in range(1,n+1):
+    sum=sum+e
+print("sum of fist",n,"natural number is",sum)
+
+
+#Q2
+n=int(input("Enter value of n"))
+sum=0
+for e in range(1,n+1):
+    sum=e**2+sum
+print("sum of squares of fist",n,"natural number is",sum)
+
+#Q3
+n=int(input("Enter value of n"))
+sum=0
+for e in range(1,n+1):
+    sum=e**3+sum
+print("sum of cubes of fist",n,"natural number is",sum)
+
+
+#Q4
+n=int(input("Enter value of n"))
+sum=0
+for e in range(1,n+1):
+    sum=(2*e-1)+sum
+print("sum of fist",n,"odd natural number is",sum)
+
+
+"""
+#Q5
+n=int(input("Enter value of n"))
+sum=0
+for e in range(1,n+1):
+    sum=(2*e)+sum
+print("sum of fist",n,"even natural number is",sum)
+
+
+
+
+
+
+
+
